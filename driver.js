fc4d0e130159de7a63f145a07e4de54680fb4a6e
scojr <180873402+scojr@users.noreply.github.com>
2025-05-10 14:23:25 -0500

    Add method calls to driver module

--- driver.js
+++ driver.js
@@ -1,7 +1,58 @@
 import Tree from "./binary-search-tree.js";
 
 const randomArray = createRandomArray(10);
-console.log(randomArray);
+const randomTree = new Tree(randomArray);
+
+console.log(randomTree.isBalanced());
+
+console.log('levelOrder');
+randomTree.levelOrder(node => {
+  console.log(node.data)
+})
+
+console.log('preOrder');
+randomTree.preOrder(node => {
+  console.log(node.data)
+})
+
+console.log('postOrder');
+randomTree.postOrder(node => {
+  console.log(node.data)
+})
+
+console.log('inOrder');
+randomTree.inOrder(node => {
+  console.log(node.data)
+})
+
+randomTree.insert(103)
+randomTree.insert(763)
+randomTree.insert(323)
+
+console.log(randomTree.isBalanced());
+randomTree.rebalance();
+console.log(randomTree.isBalanced());
+randomTree.prettyPrint();
+
+console.log('levelOrder');
+randomTree.levelOrder(node => {
+  console.log(node.data)
+})
+
+console.log('preOrder');
+randomTree.preOrder(node => {
+  console.log(node.data)
+})
+
+console.log('postOrder');
+randomTree.postOrder(node => {
+  console.log(node.data)
+})
+
+console.log('inOrder');
+randomTree.inOrder(node => {
+  console.log(node.data)
+})
 
 function createRandomArray(length) {
   const result = [];
